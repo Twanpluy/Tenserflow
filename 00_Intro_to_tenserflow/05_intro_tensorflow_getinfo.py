06537077da0ef7c15246e3a7cd4b6c147697f1c7
Twan Pluymaekers <twanpluy@gmail.com>
2023-01-05 19:57:15 +0100

    how to get basic tensor info

--- 00_Intro_to_tenserflow/05_intro_tensorflow_getinfo.py
+++ 00_Intro_to_tenserflow/05_intro_tensorflow_getinfo.py
@@ -2,3 +2,9 @@ import tensorflow as tf
 import numpy as np
 
 #Get info for a tensor
+
+# most imported info is the shape of the tensor
+# shape  = tensor.shape
+# Rank = tensor.ndim
+# Axis or Dimension = tensor[0]
+# Size = tf.size(tensor)
