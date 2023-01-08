1d850a97b661bbe0e775e9f2b674ed451bb6f171
Twan Pluymaekers <twanpluy@gmail.com>
2023-01-08 11:59:40 +0100

    Basic tensor operations

--- 00_Intro_to_tenserflow/08_it_manipulating copy 3.py
+++ 00_Intro_to_tenserflow/08_it_manipulating copy 3.py
@@ -1,2 +1,26 @@
 import tensorflow as tf
 import numpy as np
+
+# Basic Operations
+# +, -, *, /
+# create tensor:
+rank_2_tensor = tf.constant([[10,7],[3,4]])
+# +
+print(rank_2_tensor + 10)
+# *
+print(rank_2_tensor * 10)
+# -
+print(rank_2_tensor - 10)
+# /
+print(rank_2_tensor / 10)
+
+
+# we can use tensorflow built-in functions
+# by using function it is faster than using python operator
+tf.multiply(rank_2_tensor, 10)
+
+tf.add(rank_2_tensor, 10)
+
+tf.subtract(rank_2_tensor, 10)
+
+tf.divide(rank_2_tensor, 10)
