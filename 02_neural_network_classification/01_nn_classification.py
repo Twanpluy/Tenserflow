d72015240338fdee3b9b1e49a04844db590b2d88
Twan Pluymaekers <twanpluy@gmail.com>
2023-01-08 22:32:41 +0100

    Example classifications

---
+++ 02_neural_network_classification/01_nn_classification.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# input shape example multiple classification
+
+# batch size
+# width
+# height
+# color
+# shape = [None,224,224,3]
+
+#output shape example multiple classification
+
+# Sushie
+# steak
+# pizza
+# shape = [3]
+
+#inputs and outputs
