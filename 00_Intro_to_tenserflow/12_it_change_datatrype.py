800b4ca16857aa816bc26ece999d34fb1c28b119
Twan Pluymaekers <twanpluy@gmail.com>
2023-01-08 13:10:19 +0100

    Basic aggregations with random tensor

--- 00_Intro_to_tenserflow/12_it_change_datatrype.py
+++ 00_Intro_to_tenserflow/12_it_change_datatrype.py
@@ -1,2 +1,13 @@
 import tensorflow as tf
 import numpy as np
+
+# 1. Create a tensor
+B = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+C = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+
+print(B.dtype)
+print(C.dtype)
+
+# change to float16
+B = tf.cast(B, dtype=tf.float16)
+print(B.dtype)
---
+++ 00_Intro_to_tenserflow/13_it_aggr.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+
+
+# create tensor
+B = tf.constant(np.random.randint(0, 100, size=50))
+print(B)
+
+# agrr tensor = condensing them from multiple values down to a smaller number of values
+# Get absolutwe values:
+print(tf.abs(B))
+
+# Get the minimum value:
+print(f"mix: {tf.reduce_min(B)}")
+
+# Get the maximum value:
+print(f"max: {tf.reduce_max(B)}")
+
+# Get the mean value:
+print(f"mean : {tf.reduce_mean(B)}")
+
+# Get the sum value:
+print(f"sum : {tf.reduce_sum(B)}")
+
+print(f"shape: {B.shape}")
+print(f"rank: {tf.size(B)}")
+print(f"ndim: {B.ndim}")
